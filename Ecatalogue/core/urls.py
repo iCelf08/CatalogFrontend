5ebc958f4776dce28c172499f7c75c1e58a3d682
Chaamia El Farji <chfirstly@gmail.com>
2024-04-23 18:04:19 +0100

    nothing special

--- Ecatalogue/core/urls.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import routers
-
-from .views import ProductViewSet, CategoryViewSet, AuthenticateView, RegisterView
-
-router = routers.DefaultRouter()
-
-urlpatterns = router.urls
